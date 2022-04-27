3e5e1738413c09dcf89099e9866d826bc932cbe3
jacksemder <jacksemder@gmail.com>
2022-04-26 19:34:17 -0700

    First commit

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from .twitter import add_or_update_user
 from .models import DB, User, Tweet
+from .predict import predict_user
 
 
 def create_app():
@@ -53,5 +54,48 @@ def create_app():
         users = User.query.all()
         for user in users:
             add_or_update_user(user.username)
-        return render_template('base.html', title='Home', users=users)
+        return render_template(
+            'base.html', 
+            title="All users have been updated to include their latest tweets."
+        )
+    
+    @app.route('/user', methods=['POST'])
+    @app.route('/user/<user_name>', methods=['GET'])
+    def user(user_name=None, message=''):
+        try:
+            if request.method == 'POST':
+                user_name = request.values['user_name']
+                add_or_update_user(user_name)
+                message = f'User {user_name} was added succesfully'
+            tweets = User.query.filter(User.username == user_name).one().tweets
+        except Exception as e:
+            message = f'Error occurred: {e}'
+            tweets = []
+        
+        return render_template(
+            'user.html', 
+            title=user_name, 
+            tweets=tweets, 
+            message=message
+        )
+    
+    @app.route('/compare', methods=['POST'])
+    def compare():
+        user0 = request.values['user0']
+        user1 = request.values['user1']
+        hypo_tweet = request.values['tweet_text']
+
+        if user0 == user1:
+            message = 'Cannot compare a user to themselves'
+        else:
+            prediction = predict_user(user0, user1, hypo_tweet)
+            predicted_user = user0 if prediction == 0.0 else user1
+            message = f'"{hypo_tweet}" is more likely said by {predicted_user}'
+
+        return render_template(
+            'prediction.html',
+            title='Prediction',
+            message=message
+        )
+    
     return app
